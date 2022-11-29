63ac8c8fb0000887010ea5e2fcc21472b24b2bc9
joao garcia <jiboiadamor@gmail.com>
2022-11-29 17:30:32 +0000

    Handling HTTP PUT Requests

--- index.js
+++ index.js
@@ -19,15 +19,10 @@ app.get('/api/courses', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-  const schema = {
-    name: Joi.string().min(3).required()
-  };
-
-  const result = Joi.validate(req.body, schema);
-
-  if (result.error) {
+  const { error } = validateCourse(req.body); // result.error
+  if (error) {
     // 400 Bad Request
-    res.status(400).send(result.error.details[0].message);
+    res.status(400).send(error.details[0].message);
     return;
   }
 
@@ -39,8 +34,33 @@ app.post('/api/courses', (req, res) => {
   res.send(course);
 });
 
+app.put('/api/courses/:id', (req, res) => {
+  // Look up the course
+  // If not existing, return 404
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send('The course with the given ID was not found')
+  // Validate
+  // If invalid, return 400 - Bad request
 
+  const { error } = validateCourse(req.body); // result.error
+  if (error) {
+    // 400 Bad Request
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+  // Update course
+  course.name = req.body.name;
+  // Return the updated course
+  res.send(course);
+});
 
+const validateCourse = (course) => {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(course, schema);
+};
 
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
